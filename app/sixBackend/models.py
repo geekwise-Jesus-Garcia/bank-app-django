d650e22a3a794baf055395b8337cffdcc119a6db
geekwise-Jesus-Garcia <jesus1287garcia@yahoo.com>
2019-12-12 00:47:24 +0000

    updated:models & linked imports-register app

--- app/sixBackend/models.py
+++ app/sixBackend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Bank(models.Model):
+    location_name = models.CharField(max_length=100)
+
+    def _str_(self):
+        return(f" Bank {self.location_name}")
