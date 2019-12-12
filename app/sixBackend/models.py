1e6260d1216ce2c8d8383ad0013ff8de57e979ac
geekwise-Jesus-Garcia <jesus1287garcia@yahoo.com>
2019-12-12 17:44:34 +0000

    added:Account models

--- app/sixBackend/models.py
+++ app/sixBackend/models.py
@@ -6,3 +6,17 @@ class Bank(models.Model):
 
     def _str_(self):
         return(f" Bank {self.location_name}")
+
+ class Account(models.Model):
+     account_options = (
+         ('account', 'ACCOUNT')
+         ('checking', 'CHECKING')
+         ('balance', 'BALANCE')
+     )
+
+     product_username = models.CharField(max_length=100)
+     product_email = models.EmailField(max_length=100)
+     product_options = models.CharField(max_length=8)
+
+     def _str_(self):
+         return(f"Account Name {self.product_username} Account Email {self.product_email}")
