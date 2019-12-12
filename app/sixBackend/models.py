52dc779ecb32e7d160362544a85cdaf068644487
geekwise-Jesus-Garcia <jesus1287garcia@yahoo.com>
2019-12-12 17:55:37 +0000

    debug:indent error classes fix

--- app/sixBackend/models.py
+++ app/sixBackend/models.py
@@ -7,16 +7,16 @@ class Bank(models.Model):
     def _str_(self):
         return(f" Bank {self.location_name}")
 
- class Account(models.Model):
-     account_options = (
-         ('account', 'ACCOUNT')
-         ('checking', 'CHECKING')
-         ('balance', 'BALANCE')
+class Account(models.Model):
+    account_options = (
+        ('account', 'ACCOUNT'),
+        ('checking', 'CHECKING'),
+        ('balance', 'BALANCE'),
      )
+     
+    product_username = models.CharField(max_length=100)
+    product_email = models.EmailField(max_length=100)
+    product_options = models.CharField(max_length=8)
 
-     product_username = models.CharField(max_length=100)
-     product_email = models.EmailField(max_length=100)
-     product_options = models.CharField(max_length=8)
-
-     def _str_(self):
+    def _str_(self):
          return(f"Account Name {self.product_username} Account Email {self.product_email}")
